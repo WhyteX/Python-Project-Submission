5ad714bf0f4ad8253afca8e0157daf4a0a42f013
WhyteX <andi.hogea@icloud.com>
2025-12-10 21:16:11 +0000

    Added now dict for usage

--- CapstoneProject.py
+++ CapstoneProject.py
@@ -16,6 +16,7 @@ headers = {'content-type':'application/json',
             'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
             'Accept':'*/*'}
 
+dict_movie_type = {'Movie':'movie','Show':'show'}
 
 def get_country_name(abbreviation):
     try:
@@ -357,6 +358,5 @@ submit.grid(column=0, row=6, sticky="ew", padx=5, pady=5, columnspan=2)
 
 watched_movies_button = ttk.Button(Tab_Control, text='ADD TO WATCHED LIST', bootstyle='primary', command=updateWatchedMovies)
 watched_movies_button.grid(column=0, row=7, sticky="ew", padx=5, pady=5, columnspan=2)
-
 
 app.mainloop()
